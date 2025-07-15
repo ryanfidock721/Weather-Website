66ab173a8b3bd4c3ad7ff3b05d9d33a6693e7449
Ryan Fidock <ryanfidock721@gmail.com>
2025-07-15 17:12:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,19 +11,52 @@ async function fetchData() {
         }
 
         const data = await response.json();
-        console.log(data);
+        const weather = data.weather[0].icon;
+        let icon = '';
 
-        const temp = data.main.temp +'°C';
-        const feelslike = data.main.feels_like + '°C';
+        switch (weather) {
+            case '01d':
+                icon = 'https://openweathermap.org/img/wn/01d@2x.png';
+                break;
+            case '02d':
+                icon = 'https://openweathermap.org/img/wn/02d@2x.png'
+                break;
+            case '03d':
+                icon = 'https://openweathermap.org/img/wn/03d@2x.png';
+                break;
+            case '04d':
+                icon = 'https://openweathermap.org/img/wn/04d@2x.png';
+                break;
+            case '09d':
+                icon = 'https://openweathermap.org/img/wn/09d@2x.png';
+                break;
+            case '10d':
+                icon = 'https://openweathermap.org/img/wn/10d@2x.png';
+                break;
+            case '11d':
+                icon = 'https://openweathermap.org/img/wn/11d@2x.png';
+                break;
+            case '13d':
+                icon = 'https://openweathermap.org/img/wn/13d@2x.png';
+                break;
+            case '50d':
+                icon = 'https://openweathermap.org/img/wn/50d@2x.png';
+                break;
+        }
+
+        console.log(data);
+        console.log(weathericon);
+
+        const temp_max = Math.floor(data.main.temp_max) +'°C';
+        const temp_min = Math.floor(data.main.temp_min) + '°C';
+        const temp_current = Math.floor(data.main.temp) + '°C';
         const locationname = data.name;
 
-        console.log(temp);
-        console.log(feelslike);
-        console.log(locationname);
-
         document.getElementById('location').textContent = locationname;
-        document.getElementById('temperature').textContent = temp;
-        document.getElementById('feels-like').textContent = feelslike;
+        document.getElementById('temp_current').textContent = temp_current;
+        document.getElementById('temp_max').textContent = temp_max;
+        document.getElementById('temp_min').textContent = temp_min;
+        document.getElementById('weathericon').src = icon;
 
         document.getElementById('weathercontainer').classList.remove('hidden');
     }
